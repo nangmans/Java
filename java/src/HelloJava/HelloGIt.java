2d0ca6ecd892b55b1132bcb297c2227075a3e7f6
Nangman <kallusis369@gmail.com>
2018-12-21 15:59:33 +0900

    Add Boggle

--- java/src/HelloJava/HelloGIt.java
+++ java/src/HelloJava/HelloGIt.java
@@ -1,10 +1,10 @@
-package java;
+package HelloJava;
 
-public class HelloJava {
+public class HelloGIt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Hello Java");
-}
+System.out.print("Hello git");
+	}
 
 }
--- java/src/HelloJava/HelloJava.java
+++ java/src/HelloJava/HelloJava.java
@@ -1,10 +1,10 @@
-package java;
+package HelloJava;
 
-public class HelloGIt {
+public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Hello git");
-	}
+System.out.print("Hello Java");
+}
 
 }
